05251879fc8802f4ca5def5cac1f5f08cd651701
Rodion Rybko <rybkorodion201@mail.ru>
2025-06-27 01:46:57 +0700

    create header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,13 +4,52 @@ import logo from "../../../assets/logo.svg"
 
 import styles from "./header.module.css"
 
+import {NavLink} from "react-router-dom";
+import {ROUTES} from "../../../utils/routes.ts";
+
 const Header : FC = () => {
 
     return (
         <>
-            <header className="hero">
+            <header className={styles.hero}>
                 <div className="container">
-                    <img src={logo} alt="" className={styles.logo}/>
+                    <div className={styles.navbarContainer}>
+                        <img src={logo} alt="" className={styles.logo}/>
+                        <nav className={styles.navbar}>
+                            <ul className={styles.list}>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        isActive ? `${styles.link} ${styles.active}` : `${styles.navLink}`
+                                    }
+                                    to={ROUTES.MAINPAGE}>
+                                    Главная
+                                </NavLink>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        isActive ? `${styles.link} ${styles.active}` : `${styles.navLink}`
+                                    }
+                                    to={ROUTES.AUTHORIZATION}>
+                                    Рейтинг
+                                </NavLink>
+                            </ul>
+                            <ul className={styles.list}>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        isActive ? `${styles.link} ${styles.active}` : `${styles.navLink}`
+                                    }
+                                    to={ROUTES.AUTHORIZATION}>
+                                    Квизы
+                                </NavLink>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        isActive ? `${styles.link} ${styles.active}` : `${styles.navLink}`
+                                    }
+                                    to={ROUTES.REGISTRATION}>
+                                    Профиль
+                                </NavLink>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </header>
         </>
