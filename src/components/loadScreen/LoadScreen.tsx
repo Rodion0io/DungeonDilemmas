d651fa2bfccc72185d8b4a20cac7084345c0f1a0
Rodion Rybko <rybkorodion201@mail.ru>
2025-06-26 23:41:57 +0700

    add load screen

---
+++ src/components/loadScreen/LoadScreen.tsx
@@ -0,0 +1,14 @@
+import type {FC} from "react";
+
+const LoadScreen: FC = () => {
+
+    return (
+        <>
+            <div className="loader">
+
+            </div>
+        </>
+    )
+}
+
+export default LoadScreen;
